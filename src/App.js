0711a5962f2f6e38164de4942ad4b4a1bb172e46
paulgz <111ee@mail.ru>
2020-07-06 21:26:52 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [showPassword, setShowPassword] = useState(true)
+
+    return (
+        <div className="container">
+            <input 
+                type="text"
+                placeholder="Enter username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <h2>{username}</h2>
+            <h2>{showPassword ? password : '*'.repeat(password.length)}</h2>
+            <button onClick={(e) => setShowPassword(!showPassword)}>Show/Hide password</button>
+        </div>
+    )
+}
+
+export default App
